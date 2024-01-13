5d6d54a5dcdf4460103704eef7e91b349038661f
JoseCarrillo01 <josecarrival1@gmail.com>
2024-01-13 14:09:52 -0700

    Descripción de los cambios

--- src/Global/Global.js
+++ src/Global/Global.js
@@ -1,12 +1,13 @@
-var Global = {
-	playlistLimit: 50,
-	songLimit: 100,
-	access_token: "",
-	refresh_token: "",
-	redirect_uri: "https://api-spotify-98cjvmvg9-joses-projects-ab0d988f.vercel.app/",
-	client_id : "412a87451df74743b5b280ccc326b1b4",
-	client_secret : "b848a84e7fc441a89ed59e8f6ee3f99a",
-	scopes : "user-read-private user-read-email playlist-read-private"
-}
-
-export default Global;
+const Global = {
+	playlistLimit: process.env.REACT_APP_PLAYLIST_LIMIT,
+	songLimit: process.env.REACT_APP_SONG_LIMIT,
+	access_token: process.env.REACT_APP_ACCESS_TOKEN,
+	refresh_token: process.env.REACT_APP_REFRESH_TOKEN,
+	redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+	client_id: process.env.REACT_APP_CLIENT_ID,
+	client_secret: process.env.REACT_APP_CLIENT_SECRET,
+	scopes: process.env.REACT_APP_SCOPES,
+  };
+  
+  export default Global;
+  
