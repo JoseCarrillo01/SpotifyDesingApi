7e4f8437505901153fe8fe2e194dafae7f13f6c6
JoseCarrillo01 <josecarrival1@gmail.com>
2024-01-13 14:02:47 -0700

    Descripción de los cambios

--- src/Global/Global.js
+++ src/Global/Global.js
@@ -3,7 +3,7 @@ var Global = {
 	songLimit: 100,
 	access_token: "",
 	refresh_token: "",
-	redirect_uri : "https://api-spotify-gocz83vx2-joses-projects-ab0d988f.vercel.app/",
+	redirect_uri: "https://api-spotify-98cjvmvg9-joses-projects-ab0d988f.vercel.app/",
 	client_id : "412a87451df74743b5b280ccc326b1b4",
 	client_secret : "b848a84e7fc441a89ed59e8f6ee3f99a",
 	scopes : "user-read-private user-read-email playlist-read-private"
