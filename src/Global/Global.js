6cccd9464e33bbdb773f114105becc401a8edbb4
JoseCarrillo01 <josecarrival1@gmail.com>
2024-01-13 14:15:51 -0700

    Cambios Produccion despligue

--- src/Global/Global.js
+++ src/Global/Global.js
@@ -1,13 +1,12 @@
-const Global = {
-	playlistLimit: process.env.REACT_APP_PLAYLIST_LIMIT,
-	songLimit: process.env.REACT_APP_SONG_LIMIT,
-	access_token: process.env.REACT_APP_ACCESS_TOKEN,
-	refresh_token: process.env.REACT_APP_REFRESH_TOKEN,
-	redirect_uri: process.env.REACT_APP_REDIRECT_URI,
-	client_id: process.env.REACT_APP_CLIENT_ID,
-	client_secret: process.env.REACT_APP_CLIENT_SECRET,
-	scopes: process.env.REACT_APP_SCOPES,
-  };
-  
-  export default Global;
-  
+var Global = {
+	playlistLimit: 50,
+	songLimit: 100,
+	access_token: "",
+	refresh_token: "",
+	redirect_uri : "https://spotify-desing-api-2pi7.vercel.app/",
+	client_id : "412a87451df74743b5b280ccc326b1b4",
+	client_secret : "b848a84e7fc441a89ed59e8f6ee3f99a",
+	scopes : "user-read-private user-read-email playlist-read-private"
+}
+
+export default Global;
